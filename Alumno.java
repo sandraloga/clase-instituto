a4e13387db8fff76a6e5b9a84a9060f02fa22c48
Sandra <72sandraloga@gmail.com>
2016-04-03 22:47:23 +0200

    Añadido metodo addNota(int) que permite introducir una nota entera del alumno.  Este método se peude invocar tantas veces como deseemos.

--- Alumno.java
+++ Alumno.java
@@ -21,5 +21,23 @@ public class Alumno
         this.nombre= nombre;
         this.edad=edad;
     }
+    /**
+     * Método que permite introducir una nota entera del alumno.
+     * Este método se peude invocar tantas veces como deseemos.
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public void addNota(int nota)
+    {
+        int notasBis[] = new int [notas.length+1];
+        for (int index = 0 ; index <notas.length; index++)
+        {
+            notasBis[index] = notas[index];
+        }
+        notasBis[notasBis.length-1]=nota;
+        notas = notasBis;
+      
+
+    }
 
 }
