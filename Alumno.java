a7b5516d0d7eff3901c32574b1e9b33cf7af2bce
Sandra <72sandraloga@gmail.com>
2016-04-03 22:52:24 +0200

    Añadido metodo notaMedia() que permita conocer la nota media (en decimal) del alumno en este momento.

--- Alumno.java
+++ Alumno.java
@@ -21,6 +21,7 @@ public class Alumno
         this.nombre= nombre;
         this.edad=edad;
     }
+
     /**
      * Método que permite introducir una nota entera del alumno.
      * Este método se peude invocar tantas veces como deseemos.
@@ -36,8 +37,20 @@ public class Alumno
         }
         notasBis[notasBis.length-1]=nota;
         notas = notasBis;
-      
 
+    }
+
+    /**
+     * método que permita conocer la nota media (en decimal) del alumno en este momento.
+     */
+    public float notaMedia()
+    {
+        int sumaNotas=0;
+        for(int index = 0; index < notas.length; index++)
+        {
+            sumaNotas+=notas[index];
+        }
+        return sumaNotas/notas.length;
     }
 
 }
